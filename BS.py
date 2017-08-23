b2d1bc7e3d9ea0c7b103af3f2e900be42c4d7f03
while777 <msong14@hawk.iit.edu>
2017-08-23 14:41:33 -0500

    Add files via upload

--- BS.py
+++ BS.py
@@ -1,27 +1,51 @@
+""" Simulate interest rate path by the Brennan Schwartz model """
+import numpy as np
+import scipy.linalg as spl
 
-from math import log, exp, sqrt
-from numpy import maximum, cumsum, mean
-from scipy import stats
-c = 0
-def bsformula(callput, S0, K, r, T, sigma, q=0):
 
-  global c
-  c = c + 1
+def brennan_schwartz(r0, theta0, a, b, sigma, rho, y, T=0.25, N=1, seed=777):
+    np.random.seed(seed)
+    dt = T / float(N)
+    rates = [r0]
+    thetas = [theta0]
+    # Sigma = np.ones([2,2])*dt
+    # Sigma[0,1] = rho*dt
+    # Sigma[1,0] = rho*dt
+    # L = spl.cholesky(Sigma, lower=True)
 
-  d1 = (log(S0) - log(K) + (r + 0.5 * sigma ** 2) * T) / (sigma * sqrt(T))
-  d2 = (log(S0) - log(K) + (r - 0.5 * sigma ** 2) * T) / (sigma * sqrt(T))
-  if callput == 1:
-    value =  (S0 * exp( - q * T ) * stats.norm.cdf(d1, 0.0, 1.0) - K * exp( - r * T ) * stats.norm.cdf(d2,0.0,1.0))
-    delta = stats.norm.cdf(d1)
-    vega =  (S0 * exp( - q * T) * stats.norm.pdf(d1) * (( - 1 / (sqrt(T) * sigma ** 2) * (log(S0) - log(K)) - r * sqrt(T)/sigma ** 2) + 0.5 * sqrt(T)) - K * exp(-r * T) * (-1/(sqrt(T) * sigma ** 2) * (log(S0) - log(K)) - r * sqrt(T) / sigma ** 2 - 0.5 * sqrt(T)) * stats.norm.pdf(d2))
+    for i in range(N):
+        # y = np.dot(L, np.random.randn(2,1))
+        dr = (a[0] + a[1] * (thetas[-1] - rates[-1]) * dt) + sigma[0] * rates[-1] * y[0]
+        dtheta = thetas[-1] * (b[0] + b[1] * rates[-1] + b[2] * thetas[-1]) * dt + sigma[1] * thetas[-1] * y[1]  # dt * ( rho * sigma1 * Z1 + sqrt(1-rho**2) * sigma(0) * Z2)
 
-  elif callput == -1:
-    value =   K * exp(- r * T) * stats.norm.cdf(-d2, 0.0, 1.0) - S0 * exp(- q * T) * stats.norm.cdf(-d1, 0.0, 1.0)
-    delta = -stats.norm.cdf(-d1)
-    vega = (- S0 * exp( - q * T) * stats.norm.pdf( - d1) * ((1 / (sqrt(T) * sigma ** 2) * (log(S0) - log(K)) + r * sqrt(T) / sigma ** 2) - 0.5 * sqrt(T)) +  K * exp(-r * T) * (1 / (sqrt(T) * sigma ** 2) * (log(S0) - log(K)) + r * sqrt(T) / sigma ** 2 + 0.5 * sqrt(T)) * stats.norm.pdf(-d2))
+        rates.append(rates[-1] + dr)
+        thetas.append(thetas[-1] + dtheta)
 
-  tuple = ('value', value, 'delta', delta, 'vega', vega)
-  return tuple
+    return  rates
 
-print(bsformula(1, 100, 105, 0.01, 1, 0.2, 0))
-print(bsformula(-1, 100, 105, 0.01, 1, 0.2, 0))
+
+from math import *
+dt = 0.25
+Sigma = np.ones([2,2])
+rho = 0.3
+Sigma[0,0] = dt
+Sigma[0,1] = rho * dt
+Sigma[1,0] = rho * dt
+Sigma[1,1] = dt
+L = spl.cholesky(Sigma, lower=True)
+print L
+# Check that the factors multiply to Sigma
+# print np.dot(L, L.T) #transpose
+
+r = np.array([[-0.16861717,  0.4072987, -1.82467762,  0.07777817,   2.50286929],
+             [ 1.32958849,  0.20024067, -1.04039457,  0.10351275, 1.24437217]])
+
+y = np.dot(L, r)
+z = brennan_schwartz(0.02, 0.05, [0, 0.5], [0, 0.01, 0.1], [0.1, 0.05], 0.3, y, T=0.25, N=1, seed=777)
+x = z[1]
+
+mean = np.mean(x)
+std = np.std(x)
+error = float(std)/len(x)
+print z
+print mean, std, error
---
+++ BinomialAmerican.py
@@ -0,0 +1,6 @@
+import numpy as np
+from scipy import stats
+from numpy import random
+
+print np.linspace(0, 5, 6)
+
